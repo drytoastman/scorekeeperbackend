ed67d00ca212026d516eae72fc1462315f06ce8b
Brett Wilson <drytoastman@gmail.com>
2017-09-11 00:55:41 -0700

    add new datetime picker and event field tooltips

--- web/nwrsc/lib/forms.py
+++ web/nwrsc/lib/forms.py
@@ -3,7 +3,7 @@ from operator import attrgetter
 
 from flask import request, flash
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, DateField, DateTimeField, FloatField, HiddenField, PasswordField, SelectField, StringField, SubmitField
+from wtforms import BooleanField, DateField, DateTimeField, FloatField, HiddenField, PasswordField, SelectField, StringField, SubmitField, TextAreaField
 from wtforms.fields.html5 import EmailField, IntegerField, URLField
 from wtforms.validators import Email, InputRequired, Length, Optional, Required
 
@@ -159,26 +159,26 @@ class SettingsForm(MyFlaskForm):
 class EventSettingsForm(MyFlaskForm):
     eventid       = HiddenField(  'eventid')
     name          = MyStringField('Event Name',  [Length(min=4, max=32)])
-    date          = DateField('Event Date')  
-    regopened     = DateTimeField('Registration Opens')
-    regclosed     = DateTimeField('Registration Closes')
-    courses       = IntegerField('Course Count')
-    runs          = IntegerField('Run Count')    
-    countedruns   = IntegerField('Runs Counted')
-    segments      = IntegerField('Segment Count')
-    perlimit      = IntegerField('Per Driver Entry Limit')
-    sinlimit      = IntegerField('Single Driver Entry Limit')
-    totlimit      = IntegerField('Total Entry Limit')
-    conepen       = FloatField('Cone Penalty')
-    gatepen       = FloatField('Gate Penalty')
-    ispro         = BooleanField('Is A ProSolo') 
-    ispractice    = BooleanField('Is A Practice')
-    location      = MyStringField('Location')
-    sponsor       = MyStringField('Sponsor')
-    host          = MyStringField('Host')
-    chair         = MyStringField('Chair')
-    cost          = FloatField('Cost', [Optional()])
-    payments      = MyStringField('Payment Account')
-    notes         = MyStringField('Notes')
+    date          = DateField(    'Event Date',             render_kw={'title':'The date of the event'})  
+    regopened     = DateTimeField('Registration Opens',     render_kw={'title':'When registration should open'}, format='%Y-%m-%d %H:%M')
+    regclosed     = DateTimeField('Registration Closes',    render_kw={'title':'When registration should close'}, format='%Y-%m-%d %H:%M')
+    perlimit      = IntegerField( 'Per Driver Entry Limit', render_kw={'title':'Limit to the number of entries a single driver can register (0=nolimit)'})
+    totlimit      = IntegerField( 'Total Entry Limit',      render_kw={'title':'The total limit for all registrations for the event (0=nolimit)'})
+    cost          = FloatField(   'Cost', [Optional()],     render_kw={'title':'The cost to prepay for the event'})
+    payments      = MyStringField('Payment Account',        render_kw={'title':'TBD: the payment account information for taking prepayments'})
+    notes         = TextAreaField('Notes',                  render_kw={'title':'Notes for the event that show up on the registation page', 'rows':6})
+    ispro         = BooleanField( 'Is A ProSolo',           render_kw={'title':'Check if this is a ProSolo style event'}) 
+    ispractice    = BooleanField( 'Is A Practice',          render_kw={'title':'Check if this is a practice and not counted towards championship points'})
+    location      = MyStringField('Location',               render_kw={'title':'The location of the event'})
+    sponsor       = MyStringField('Sponsor',                render_kw={'title':'The event sponsor'})
+    host          = MyStringField('Host',                   render_kw={'title':'The event host'})
+    chair         = MyStringField('Chair',                  render_kw={'title':'The event chair'})
+    sinlimit      = IntegerField( 'Singles Entry Limit',    render_kw={'title':'When allowing multiple entries, a limit to the number of individual people that can register (0=nolimit)'})
+    courses       = IntegerField( 'Course Count',           render_kw={'title':'The number of courses in the event'})
+    runs          = IntegerField( 'Run Count',              render_kw={'title':'The number of runs in the event'})    
+    countedruns   = IntegerField( 'Runs Counted',           render_kw={'title':'The number of runs counted towards results (0=all)'})
+    segments      = IntegerField( 'Segment Count',          render_kw={'title':'The number of segments on each course'})
+    conepen       = FloatField(   'Cone Penalty',           render_kw={'title':'The penalty value for hitting a cone'})
+    gatepen       = FloatField(   'Gate Penalty',           render_kw={'title':'The penalty value for missing a gate'})
     submit        = SubmitField(  'Update')
 
