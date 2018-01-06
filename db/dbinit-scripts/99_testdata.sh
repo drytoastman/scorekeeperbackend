af91b5793f1e89ae9e25ba0b1a1ac1ba05d799ca
Brett Wilson <drytoastman@gmail.com>
2018-01-05 20:16:42 -0800

    build testdata from scratch sql file instead

--- db/dbinit-scripts/99_testdata.sh
+++
@@ -1,7 +0,0 @@
-SERIES=pro2017
-if [ -f /tmp/$SERIES.pgdump ]; then
-    psql -U postgres -c "CREATE ROLE $SERIES WITH PASSWORD '$SERIES'"
-    psql -U postgres -c "GRANT $SERIES TO localuser"
-    pg_restore -d postgres -U postgres -C -c -e /tmp/$SERIES.pgdump
-fi
-
