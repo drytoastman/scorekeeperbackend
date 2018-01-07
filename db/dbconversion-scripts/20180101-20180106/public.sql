637ee5b4aee05c67a8c51af767d54e8368593b58
Brett Wilson <drytoastman@gmail.com>
2018-01-07 13:52:18 -0800

    add created column to cars and events too in case we decide to use the more complex merge process with them as well later down the road

--- db/dbconversion-scripts/20180101-20180106/public.sql
+++ db/dbconversion-scripts/20180101-20180106/public.sql
@@ -1,1 +1,3 @@
 ALTER TABLE drivers ADD COLUMN created NOT NULL DEFAULT now();
+ALTER TABLE events  ADD COLUMN created NOT NULL DEFAULT now();
+ALTER TABLE cars    ADD COLUMN created NOT NULL DEFAULT now();
