7646bf1be28b10f1d8f32b38e2e2d31b5fd7898b
Brett Wilson <drytoastman@gmail.com>
2018-04-13 16:51:46 -0700

    up the transaction timeout as we were hitting it, just a safety in case something runs into an infinite loop

--- sync/synclogic/model.py
+++ sync/synclogic/model.py
@@ -148,7 +148,7 @@ class DataInterface(object):
                 address = server.address or server.hostname
             db = psycopg2.connect(host=address, user=user, password=password, connect_timeout=server.ctimeout, **args)
             with db.cursor() as cur:
-                cur.execute("set idle_in_transaction_session_timeout=5000")
+                cur.execute("set idle_in_transaction_session_timeout=60000")
             return db
         except:
             server.recordConnectFailure()
