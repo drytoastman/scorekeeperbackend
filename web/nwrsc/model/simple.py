5ef71da30a261f361733f38e00afd907c6cc7f74
Brett M. Wilson <drytoastman@gmail.com>
2018-05-05 15:30:19 -0700

    most things calling Registration.getForEvent assuming in numerical order so set it that way, fixes grid number order and current registration list

--- web/nwrsc/model/simple.py
+++ web/nwrsc/model/simple.py
@@ -149,7 +149,7 @@ class Registration(AttrBase):
     @classmethod
     def getForEvent(cls, eventid, paymentRequired=False):
         with g.db.cursor() as cur:
-            cur.execute("SELECT d.firstname,d.lastname,d.email,d.barcode,c.*,r.*,r.modified as regmodified, d.attr as dattr,c.attr as cattr FROM cars c JOIN drivers d ON c.driverid=d.driverid JOIN registered r ON r.carid=c.carid WHERE r.eventid=%s", (eventid,))
+            cur.execute("SELECT d.firstname,d.lastname,d.email,d.barcode,c.*,r.*,r.modified as regmodified, d.attr as dattr,c.attr as cattr FROM cars c JOIN drivers d ON c.driverid=d.driverid JOIN registered r ON r.carid=c.carid WHERE r.eventid=%s ORDER BY c.number", (eventid,))
             retdict = {x['carid']:Entrant(**x, payments=[]) for x in cur.fetchall()}
 
             cur.execute("SELECT * FROM payments WHERE eventid=%s", (eventid,))
