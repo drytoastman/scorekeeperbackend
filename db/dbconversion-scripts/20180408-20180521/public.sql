7a16df1bb2d828c438ae7a2f864f197b410c0b82
Brett Wilson <drytoastman@gmail.com>
2018-05-25 01:03:35 -0700

    record the schema conversions so far

---
+++ db/dbconversion-scripts/20180408-20180521/public.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION updatechecks() RETURNS TRIGGER AS $body$
+DECLARE
+    app text := current_setting('application_name');
+    oldrow hstore := hstore(OLD);
+    newrow hstore := hstore(NEW);
+BEGIN
+    IF (TG_OP = 'UPDATE') THEN
+        IF (OLD = NEW) THEN
+            RETURN NULL;
+        END IF;
+        FOR ii IN 0..TG_NARGS LOOP
+            IF (oldrow -> TG_ARGV[ii] != newrow -> TG_ARGV[ii]) THEN
+                RAISE EXCEPTION 'Cannot use UPDATE to change % of the primary key', TG_ARGV[ii];
+            END IF;
+        END LOOP;
+        IF oldrow -> 'modified' = newrow -> 'modified' THEN
+            RAISE EXCEPTION 'Updating without changing modification time';
+        END IF;
+        IF akeys(newrow - oldrow) = ARRAY['modified'] THEN
+            RETURN NULL;
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$body$
+LANGUAGE plpgsql;
