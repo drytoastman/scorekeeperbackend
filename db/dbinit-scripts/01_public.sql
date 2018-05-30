d4acc99b918da2b0c352307612240e6d920f2361
Brett Wilson <drytoastman@gmail.com>
2018-05-29 23:24:13 -0700

    add an index to the local eventstream time column

--- db/dbinit-scripts/01_public.sql
+++ db/dbinit-scripts/01_public.sql
@@ -273,6 +273,7 @@ CREATE TABLE localeventstream (
     event JSONB     NOT NULL,
     time  TIMESTAMP NOT NULL
 );
+CREATE INDEX ON localeventstream(time);
 REVOKE ALL   ON localeventstream FROM public;
 GRANT  ALL   ON localeventstream TO mergeaccess;
 COMMENT ON TABLE localeventstream IS 'Local events useful for serving up to the announcer interface, not merged';
