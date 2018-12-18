a60606b9237e00ddd3112c9ddd2b2a946a6d3032
Brett Wilson <drytoastman@gmail.com>
2018-12-18 04:11:58 -0800

    don't die if a cron job fails

--- common/sccommon/cronjobs.py
+++ common/sccommon/cronjobs.py
@@ -1,16 +1,24 @@
+import logging
 import schedule
 import subprocess
 import time
 
 def crondaemon():
     def webcron():
-        from nwrsc.app import cron_jobs
-        cron_jobs()
+        try:
+            from nwrsc.app import cron_jobs
+            cron_jobs()
+        except Exception as e:
+            logging.getLogger("webcron").error("Failed ro run webcron: %s", e)
+            
 
     def backup():
-        from sccommon.backuprestore import backup_db
-        backup_db('scorekeeperbackup')
-
+        try:
+            from sccommon.backuprestore import backup_db
+            backup_db('scorekeeperbackup')
+        except Exception as e:
+            logging.getLogger("backup").error("Failed ro run backup: %s", e)
+ 
     # UTC
     schedule.every().day.at("11:00").do(webcron)
     schedule.every().day.at("11:30").do(backup)
