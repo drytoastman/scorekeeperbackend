ec67861ef78c1bb89dddbff384cbd7573ee09172
Brett Wilson <drytoastman@gmail.com>
2019-05-16 00:30:50 -0700

    move cron functions outside daemon so its easier to test

--- common/sccommon/cronjobs.py
+++ common/sccommon/cronjobs.py
@@ -29,39 +29,39 @@ def queueEmail(body):
             cur.execute("INSERT INTO emailqueue (content) VALUES (%s)", (json.dumps(email),))
 
 
+def webcron():
+    try:
+        from nwrsc.app import cron_jobs
+        cron_jobs()
+    except Exception as e:
+        logging.getLogger("webcron").error("Failed ro run webcron: %s", e)
+
+
+def backup():
+    try:
+        from sccommon.backuprestore import backup_db
+        backup_db('scorekeeperbackup')
+    except Exception as e:
+        logging.getLogger("backup").error("Failed ro run backup: %s", e)
+
+
+def mailerrors():
+    try:
+        from sccommon.logging import collect_errors
+        errors = collect_errors()
+        txt = list()
+        for k in sorted(errors.keys()):
+            txt.append('\nFILE: {}\n'.format(k))
+            txt.extend(errors[k])
+    except Exception as e:
+        txt = ["Failed to collect errors: {}".format(e)]
+
+    queueEmail('\n'.join(txt))
+
+
 def crondaemon():
     logging_setup('/var/log/sccron.log')
 
-    def webcron():
-        try:
-            from nwrsc.app import cron_jobs
-            cron_jobs()
-        except Exception as e:
-            logging.getLogger("webcron").error("Failed ro run webcron: %s", e)
-            
-
-    def backup():
-        try:
-            from sccommon.backuprestore import backup_db
-            backup_db('scorekeeperbackup')
-        except Exception as e:
-            logging.getLogger("backup").error("Failed ro run backup: %s", e)
-
-
-    def mailerrors():
-        try:
-            from sccommon.logging import collect_errors
-            errors = collect_errors()
-            txt = list()
-            for k in sorted(errors.keys()):
-                txt.append('\nFILE: {}\n'.format(k))
-                txt.extend(errors[k])
-        except Exception as e:
-            txt = ["Failed to collect errors: {}".format(e)]
-
-        queueEmail('\n'.join(txt))
-
- 
     # UTC
     schedule.every().day.at("08:00").do(backup)  # 1:00 AM PDT
     schedule.every().day.at("11:00").do(webcron) # 4:00 AM PDT
