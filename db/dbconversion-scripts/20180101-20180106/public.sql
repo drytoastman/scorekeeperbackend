c9d87fb4dc78c73aad8faeb00b20a3d95edf7d88
Brett Wilson <drytoastman@gmail.com>
2019-10-23 17:21:30 -0700

    remove old conversions, noone should be using those as they wouldn't be able to sync

--- db/dbconversion-scripts/20180101-20180106/public.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE drivers ADD COLUMN created TIMESTAMP NOT NULL DEFAULT now();
-UPDATE drivers SET created='epoch';
--- db/dbconversion-scripts/20180101-20180106/series.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE events  ADD COLUMN created TIMESTAMP NOT NULL DEFAULT now();
-ALTER TABLE cars    ADD COLUMN created TIMESTAMP NOT NULL DEFAULT now();
-UPDATE events SET created='epoch';
-UPDATE cars SET created='epoch';
