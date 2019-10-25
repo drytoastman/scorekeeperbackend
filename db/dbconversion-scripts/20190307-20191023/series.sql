d92bf53d3528f0c7cc202f3fb8972dbdcb7fcd6f
Brett Wilson <drytoastman@gmail.com>
2019-10-25 00:32:07 -0700

    make sure conversion ensures the special classes we want and purges out the hold HOLD class

--- db/dbconversion-scripts/20190307-20191023/series.sql
+++ db/dbconversion-scripts/20190307-20191023/series.sql
@@ -5,3 +5,13 @@ DROP TRIGGER classordercon ON classorder;
 CREATE CONSTRAINT TRIGGER classordercon AFTER INSERT OR UPDATE ON classorder DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE classorderconstraints('<seriesname>');
 
 ALTER TABLE events ADD COLUMN spclasses TEXT[] NOT NULL DEFAULT '{}';
+
+DELETE FROM registered WHERE carid IN (SELECT carid FROM cars WHERE classcode='HOLD');
+DELETE FROM runs       WHERE carid IN (SELECT carid FROM nwr2019.cars WHERE classcode='HOLD');
+DELETE FROM cars       WHERE classcode='HOLD';
+DELETE FROM classlist  WHERE classcode='HOLD';
+
+INSERT INTO classlist (classcode, descrip, indexcode, caridxrestrict, classmultiplier, carindexed, usecarflag, eventtrophy, champtrophy, secondruns, countedruns) VALUES ('_HOLD', 'Unknown Class', '', '', 1.0, 'f', 'f', 'f', 'f', 'f', 0);
+INSERT INTO classlist (classcode, descrip, indexcode, caridxrestrict, classmultiplier, carindexed, usecarflag, eventtrophy, champtrophy, secondruns, countedruns) VALUES ('_AM',   'AM Session',    '', '', 1.0, 'f', 'f', 't', 'f', 'f', 0);
+INSERT INTO classlist (classcode, descrip, indexcode, caridxrestrict, classmultiplier, carindexed, usecarflag, eventtrophy, champtrophy, secondruns, countedruns) VALUES ('_PM',   'PM Session',    '', '', 1.0, 'f', 'f', 't', 'f', 'f', 0);
+INSERT INTO classlist (classcode, descrip, indexcode, caridxrestrict, classmultiplier, carindexed, usecarflag, eventtrophy, champtrophy, secondruns, countedruns) VALUES ('_DAY',  'Day Session',   '', '', 1.0, 'f', 'f', 't', 'f', 'f', 0);
