92094c633ff32a1122d6dcda25aaffcbcb4f92a4
Brett Wilson <drytoastman@gmail.com>
2019-11-06 23:37:33 -0800

    generate CA and server certs and keys

---
+++ compositions/certgen.sh
@@ -0,0 +1,4 @@
+openssl req  -new -nodes -text -out root.csr   -keyout root.key   -subj "/CN=root.wwscc.org"
+openssl req  -new -nodes -text -out server.csr -keyout server.key -subj "/CN=host.wwscc.org"
+openssl x509 -req -in root.csr   -text -days 3650 -extfile certgen.cnf -extensions v3_ca     -signkey root.key -out root.cert
+openssl x509 -req -in server.csr -text -days 3650 -extfile certgen.cnf -extensions v3_server -CA root.cert -CAkey root.key -CAcreateserial -out server.cert
